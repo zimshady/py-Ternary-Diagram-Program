edb1c28a44f7467b97a4bc3bd538a8a5860a32ab
Ton <zimshady6@gmail.com>
2016-11-04 21:26:30 +0000

    Main program
    The main ternary plotter program

---
+++ Ternary_plotter.py
@@ -0,0 +1,19 @@
+import ternary
+from matplotlib import *
+
+### Scatter Plot
+scale = 100
+figure, tax = ternary.figure(scale=scale)
+tax.set_title("Scatter Plot", fontsize=20)
+tax.boundary(linewidth=2.0)
+tax.gridlines(multiple=5, color="blue")
+
+# Plot a few different styles with a legend
+Brentonpoints = [(float(11.82),float(80),float(8.81))] #has to be a tuple in a list (lithics,quartz,feldspar)
+tax.scatter(Brentonpoints, marker='s', color='red', label="Brenton")
+Mikepoints = [(float(11.82),float(76.09),float(10.77))]
+tax.scatter(Mikepoints, marker='D', color='green', label="Mike")
+tax.legend()
+tax.ticks(axis='lbr', linewidth=1, multiple=5)
+
+tax.show()
