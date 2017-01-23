b3e820fb8ad03ee91e2f5cdcfa02134028f05200
Ton <zimshady6@gmail.com>
2017-01-23 18:25:58 +0000

    added csvfile.close() and code for more data

--- Ternary_plotter_Pettijohn_Arenite_Classification.py
+++ Ternary_plotter_Pettijohn_Arenite_Classification.py
@@ -36,8 +36,36 @@ with open(path + "\\" + "all_ternary_data.csv") as csvfile:
     list2 = list1.pop(0)
     list1 = [map(float, x) for x in list1]
     tup_percent = zip (*list1)
+    csvfile.close()
+
+with open(path + "\\" + "all_Dingle_point_data.csv") as csvfile:
+    reader = csv.reader(csvfile, delimiter = ',')
+    list3 = []
+
+    for row in reader:
+        list3.append(row)
+
+    list4 = list3.pop(0)
+    list3 = [map(float, x) for x in list3]
+    tup_percent2 = zip (*list3)
+    csvfile.close()
+
+with open(path + "\\" + "all_WMunster_point_data.csv") as csvfile:
+    reader = csv.reader(csvfile, delimiter = ',')
+    list5 = []
+
+    for row in reader:
+        list5.append(row)
+
+    list6 = list5.pop(0)
+    list5 = [map(float, x) for x in list5]
+    tup_percent3 = zip (*list5)
+    csvfile.close()
 
 count=0
+count1=0
+count2=0
+
 cmap=get_cmap(len(list2)+1)
 #Boundary lines for Pettijohn 1987 arenite classification:
 qtzAren1, qtzAren2, qtzAren3 = (0,95,5),(5,90,5),(5,95,0)
@@ -47,8 +75,18 @@ noQtz = (50,0,50)
 #has to be a tuple in a list (lithics,quartz,feldspar)
 for i in tup_percent:
     print "i=" + str(i)
-    tax.scatter([i], marker='p', color='black', label=list2[count])
+    tax.scatter([i], marker='p', color='red', label=list2[count])
     count+=1
+
+for i in tup_percent2:
+    print "j=" + str(i)
+    tax.scatter([i], marker='*', color='black', label=list4[count1])
+    count1+=1
+
+for i in tup_percent3:
+    print "j=" + str(i)
+    tax.scatter([i], marker='*', color='black', label=list6[count2])
+    count2+=1
 
 tax.line(qtzAren1,qtzAren2,linewidth=1.0,color='black')
 tax.line(qtzAren3,qtzAren2,linewidth=1.0,color='black')
